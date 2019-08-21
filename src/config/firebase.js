9635d68e6aea8fd9a38c7f36df54ae2aa3bce4c0
princeivankent <princeivankentmtiburcio@gmail.com>
2019-08-21 22:44:34 +0800

    commented findmylocation function

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase'
 import 'firebase/firestore'
 
 const config = {
-  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
-  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.VUE_APP_FIREBASE_API_ID
+  apiKey: "AIzaSyCzbCxRk9ognu8iXctoegk1ptf87toSVr0",
+  authDomain: "socialite-pkdev.firebaseapp.com",
+  databaseURL: "https://socialite-pkdev.firebaseio.com",
+  projectId: "socialite-pkdev",
+  storageBucket: "",
+  messagingSenderId: "77660681027",
+  appId: "1:77660681027:web:4a146dd36120722b"
 }
 
 firebase.initializeApp(config);
